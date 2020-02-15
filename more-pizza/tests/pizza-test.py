72fe9936c8ad2451b671ec62af9b9ef49628c52e
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-02-15 21:03:26 +0700

    Add unit test for PizzaCalculator

--- more-pizza/tests/pizza-test.py
+++ more-pizza/tests/pizza-test.py
@@ -1,9 +1,18 @@
 import unittest
-from pizza import Pizza
+from pizza import Pizza, PizzaCalculator
 
-class PizzaTest(unittest.TestCase):
-    def test_pizza_can_contain_slice(self):
-        pizza_a = Pizza(0, 5)
-        self.assertEqual(pizza_a.slices, 5)
+class PizzaCalculatorTest(unittest.TestCase):
+    def test_should_return_pizza_index_0_2_3(self):
+        target_slice = 17
+        n_pizza = 4
+        pizza_types = [
+            Pizza(0, 2),
+            Pizza(1, 5),
+            Pizza(2, 6),
+            Pizza(3, 8)
+        ]
+        pizza_index = PizzaCalculator.get_pizza(pizza_types, target_slice)
+        self.assertEqual(3, len(pizza_index))
+        self.assertEqual("0, 2, 3", ", ".join(pizza_index))
 
 unittest.main()
